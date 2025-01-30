b3bc71618f1f9667c1d0557e7bec898f4ec19f58
AlessandroColombo0 <alcobplayer225.com@gmail.com>
2025-01-30 14:46:51 +0100

    3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="your_package",  # Replace with your actual package name
+    name="pr",  # Replace with your actual package name
     version="0.1",
     packages=find_packages(),
     install_requires=requirements,  # Use dependencies from requirements.txt
