e81c343ecc9132f8a25ed63b62f2af648481d2a3
AlessandroColombo0 <alcobplayer225.com@gmail.com>
2025-01-30 15:00:35 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup, find_packages
 
-# Read dependencies from requirements.txt
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
 setup(
-    name="pr",  # Replace with your actual package name
+    name="pr",
     version="0.1",
     packages=find_packages(),
-    install_requires=requirements,  # Use dependencies from requirements.txt
+    install_requires=[
+        'asttokens>=3.0.0',
+        'colorama>=0.4.6',
+        'executing>=2.2.0',
+        'numpy>=2.2.2',
+        'pandas>=2.2.3',
+        'Pygments>=2.19.1',
+        'python-dateutil>=2.9.0',
+        'pytz>=2024.2',
+        'regex>=2024.11.6',
+        'setuptools>=75.8.0',
+        'six>=1.17.0',
+        'tzdata>=2025.1'
+    ]
 )
